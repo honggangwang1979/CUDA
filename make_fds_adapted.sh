bfc8467b42a7653c864455b2266b3492015e5b19
root <honggangwang1979@gmail.com>
2023-09-24 02:42:23 +0000

    add setup shell used in paperspace Free-P5000 GPU

---
+++ make_fds_adapted.sh
@@ -0,0 +1,1 @@
+mpifort -c -m64 -O0  -Wall -Werror  -g -traceback -Mrecursive -stdpar -cpp -DGITHASH_PP=\"-master\" -DGITDATE_PP=\""Tue Aug 29 11:28:56 2023 -0400\"" -DBUILDDATE_PP=\""Sep 21, 2023  16:58:09\"" -DCOMPVER_PP=\""Gnu gfortran 9.4.0-1ubuntu1~20.04.2)"\"   ../../Source/prec.f90
